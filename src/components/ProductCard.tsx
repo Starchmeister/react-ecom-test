f8ac794c9c4c6833e13b77d83812822d2575e99d
starchmeister <slulekobiyelaza@gmail.com>
2026-02-19 16:00:44 +0200

    fixed tag

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,7 +13,7 @@ product:{
 export default function ProductCard({product}: ProductCardProps) {
     return(
         <div className="product-card" key={product.id}>
-            <img src={product.image} alt="product" className="product-card-image" />
+            <img src={product.image} alt={product.name} className="product-card-image" />
             <div className="product-card-content">
                 <h3 className="product-card-name">{product.name}</h3>
                 <p className="product-card-price">{product.price}</p>
