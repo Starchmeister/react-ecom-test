4465200ff28e35db8e1c56cb3ee2767b8b079350
starchmeister <slulekobiyelaza@gmail.com>
2026-02-19 15:49:17 +0200

    added home page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '5e21-2a09-bac5-d4bf-555-00-88-15.ngrok-free.app',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
